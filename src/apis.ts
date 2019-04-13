f09f61607750d5636d8af9f6e64ed2490412fb68
Allen Kim <allenbkim@gmail.com>
2019-04-13 14:06:22 -0700

    feat: add POST body parameter to analyze endpoint.

--- src/apis.ts
+++ src/apis.ts
@@ -11,22 +11,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/analyze', async (req, res, next) => {
-  const text = `
-Verrières is sheltered on the north by a high mountain which is one of
-the branches of the Jura. The jagged peaks of the Verra are covered
-with snow from the beginning of the October frosts. A torrent which
-rushes down from the mountains traverses Verrières before throwing
-itself into the Doubs, and supplies the motive power for a great number
-of saw mills. The industry is very simple, and secures a certain
-prosperity to the majority of the inhabitants who are more peasant than
-bourgeois. It is not, however, the wood saws which have enriched this
-little town. It is the manufacture of painted tiles, called Mulhouse
-tiles, that is responsible for that general affluence which has caused
-the façades of nearly all the houses in Verrières to be rebuilt since
-the fall of Napoleon.
-`;
+  let analyzeText;
+  if (req.body.analyze_text)
+    analyzeText = req.body.analyze_text;
+  else
+    analyzeText = `
+      Verrières is sheltered on the north by a high mountain which is one of
+      the branches of the Jura. The jagged peaks of the Verra are covered
+      with snow from the beginning of the October frosts. A torrent which
+      rushes down from the mountains traverses Verrières before throwing
+      itself into the Doubs, and supplies the motive power for a great number
+      of saw mills. The industry is very simple, and secures a certain
+      prosperity to the majority of the inhabitants who are more peasant than
+      bourgeois. It is not, however, the wood saws which have enriched this
+      little town. It is the manufacture of painted tiles, called Mulhouse
+      tiles, that is responsible for that general affluence which has caused
+      the façades of nearly all the houses in Verrières to be rebuilt since
+      the fall of Napoleon.
+      `;
 
-  const { documentSentiment, sentences } = await analyze(text) as any;
+  const { documentSentiment, sentences } = await analyze(analyzeText) as any;
   const voicePromise = sentences.map(({ text }, idx) => {
     text.$requestId = idx;
 
