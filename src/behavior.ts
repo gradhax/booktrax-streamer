51a71e569e765d71d16c43fabe01e07531f7c9f1
Allen Kim <allenbkim@gmail.com>
2019-04-14 02:25:38 -0700

    fix: handle multiple carriage returns

--- src/behavior.ts
+++ src/behavior.ts
@@ -11,7 +11,10 @@ export default (socket) => {
 `The little town of Verrières can pass for one of the prettiest in Franche-Comté. Its white houses with their pointed red-tiled roofs stretch along the slope of a hill, whose slightest undulations are marked by groups of vigorous chestnuts. The Doubs flows to within some hundred feet above its fortifications, which were built long ago by the Spaniards, and are now in ruins. 
 Verrières is sheltered on the north by a high mountain which is one of the branches of the Jura. The jagged peaks of the Verra are covered with snow from the beginning of the October frosts. A torrent which rushes down from the mountains traverses Verrières before throwing itself into the Doubs, and supplies the motive power for a great number of saw mills. The industry is very simple, and secures a certain prosperity to the majority of the inhabitants who are more peasant than bourgeois. It is not, however, the wood saws which have enriched this little town. It is the manufacture of painted tiles, called Mulhouse tiles, that is responsible for that general affluence which has caused the façades of nearly all the houses in Verrières to be rebuilt since the fall of Napoleon.`;
 
-  const paragraphs = analyzeText.split('\n');
+  const splitText = analyzeText.split('\n');
+  const paragraphs = splitText.filter(split => {
+    return split && split.trim().length > 0;
+  });
   const promises = paragraphs.map(async function(paragraph) {
     const { documentSentiment, sentences } = await analyze(paragraph) as any;
     const voicePromise = sentences.map(({ text }, idx) => {
