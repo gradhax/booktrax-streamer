29d39551721cd2563b4c6364c9e2031df5cfab0d
eldeni <eldeniyenden@gmail.com>
2019-04-13 20:51:41 -0700

    feat: make argument from `request-analyze` event taken

--- src/behavior.ts
+++ src/behavior.ts
@@ -1,19 +1,13 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-
 import { analyze, analyzeEntities } from '@@modules/nl';
-import { synthesize } from '@@modules/polly';
 import { getRandomGif } from '@@modules/giphy';
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { synthesize } from '@@modules/polly';
+import log from '@@modules/log';
 
-const app = express();
-const router = express.Router();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+export default (socket) => {
+  socket.on('request-analyze', async (text) => {
+    log('[socket] request-analyze %o', text);
 
-router.post('/analyze', async (req, res, next) => {
-  const { analyze_text } = req.body;
-  const analyzeText = analyze_text ||
+    const analyzeText = text ||
 `The little town of Verrières can pass for one of the prettiest in Franche-Comté. Its white houses with their pointed red-tiled roofs stretch along the slope of a hill, whose slightest undulations are marked by groups of vigorous chestnuts. The Doubs flows to within some hundred feet above its fortifications, which were built long ago by the Spaniards, and are now in ruins. 
 Verrières is sheltered on the north by a high mountain which is one of the branches of the Jura. The jagged peaks of the Verra are covered with snow from the beginning of the October frosts. A torrent which rushes down from the mountains traverses Verrières before throwing itself into the Doubs, and supplies the motive power for a great number of saw mills. The industry is very simple, and secures a certain prosperity to the majority of the inhabitants who are more peasant than bourgeois. It is not, however, the wood saws which have enriched this little town. It is the manufacture of painted tiles, called Mulhouse tiles, that is responsible for that general affluence which has caused the façades of nearly all the houses in Verrières to be rebuilt since the fall of Napoleon.`;
 
@@ -44,37 +38,15 @@ Verrières is sheltered on the north by a high mountain which is one of the bran
     };
   });
 
-  const result = await Promise.all(promises);
+  const payload = await Promise.all(promises);
+  // console.log('payload', payload);
 
-  console.log('result', result);
-
-  res.send(result);
-  
-  // const { documentSentiment, sentences } = await analyze(analyzeText) as any;
-  // const voicePromise = sentences.map(({ text }, idx) => {
-  //   text.$requestId = idx;
-  //   return synthesize({
-  //     requestId: idx,
-  //     text: text.content,
-  //   });
-  // });
-
-  // const voices = await Promise.all(voicePromise);
-  // const entity = await analyzeEntities(analyzeText);
-  // const gifUrl = await getRandomGif(entity.name);
-
-  // res.send({
-  //   analyze: {
-  //     documentSentiment,
-  //     sentences,
-  //   },
-  //   entity: {
-  //     name: entity.name,
-  //     salience: entity.salience,
-  //     gifUrl: gifUrl,
-  //   },
-  //   voices,
-  // });
-});
-
-export default () => router;
+  socket.emit(
+    'response-analyze',
+    payload,
+    () => {
+      log('acknowledgement, done()');
+    },
+  );
+  });
+};
--- src/modules/io.ts
+++ src/modules/io.ts
@@ -4,29 +4,32 @@ const state = {
   io: null,
 };
 
-export function init(server) {
+export function init(server, behavior) {
   const io = socketIO(server);
   io.on('connection', (socket) => {
     const ts = new Date().toISOString();
     const socketId = socket.id;
     const origin = socket.handshake.headers.origin;
     console.log(
-`
-%s io.init(): connection succeeds,
-origin: %s
-id: %s
-`,
+      `%s io.init(): connection succeeds, origin: %s id: %s`,
       ts,
       socket.handshake.headers.origin,
       socket.id,
     );
 
     state.io = io;
-    socket.emit('connection-result', {
+    socket.emit('response-connection', {
       origin,
       socketId,
     });
+
+    socket.emit('debug', 'just for debugging');
+    io.to(socket.id).emit('debug', `debugging (private msg) to ${socket.id}`);
+
+    behavior && behavior(socket);
   });
+
+  io.sockets.emit('debug', 'msg for everyone');
 }
 
 export function getIO() {
