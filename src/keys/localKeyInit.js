665ed58e3e37cc22bca5bde7f4d4a864bcd28c61
eldeni <eldeniyenden@gmail.com>
2019-05-21 21:56:18 +0900

    refactor: remove localKeyInit

--- src/keys/localKeyInit.js
+++
@@ -1,6 +0,0 @@
-module.exports = () => process.env = {
-  ...process.env,
-  AWS_ACCESS_KEY_ID: 'AKIAWSNGXXMDCCFEUMPN',
-  AWS_SECRET_ACCESS_KEY: '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',
-  GIPHY_API_KEY: '7GfpkrSgSGIqxkSe1KFRvmVaparH0MYA',
-};
